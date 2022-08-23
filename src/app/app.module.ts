9ff6119c05afea1b4ec1112a59cc60bc47050893
Tiago-Pessoa <tiago.pessoa@cpb.com.br>
2022-08-23 08:45:36 -0300

    next estage of project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { MaterialModule } from './material/material.module';
+import { MessagesComponent } from './messages/messages.component';
+import { NgModule } from '@angular/core';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MaterialModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,20 @@
+import { Hero } from "./hero.model";
+
+export const HEROES: Hero[] = [
+  {id:1, name: "Homem Aranha"},
+  {id:2, name: "Homem Gelo"},
+  {id:3, name: "Homem Invisível"},
+  {id:4, name: "Homem Morcego"},
+  {id:5, name: "Homem Pedra"},
+  {id:6, name: "Homem Fumaça"},
+  {id:7, name: "Homem Lontra"},
+  {id:8, name: "Homem Crocodilo"},
+  {id:9, name: "Homem Sombra"},
+  {id:10, name: "Homem Homem"},
+  {id:11, name: "Homem Papagaio"},
+  {id:12, name: "Homem Texugo"},
+  {id:13, name: "Homem Capivara"},
+  {id:14, name: "Homem Jumento"},
+  {id:15, name: "Homem Carrapato"},
+];
+
