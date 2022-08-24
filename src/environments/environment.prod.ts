5eef7ba205a728535a19dfb4181af0e77d625dab
Tiago-Pessoa <tiago.pessoa@cpb.com.br>
2022-08-24 15:42:39 -0300

    Teste

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://toh-json-server-api.herokuapp.com/api'
+  // baseUrl: 'https://toh-json-server-api.herokuapp.com/api'
+  baseUrl: 'http://localhost:3000/api'
 };
