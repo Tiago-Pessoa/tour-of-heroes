e18ccfcbbd5527d4a3866651bc8e9bd1e518ef68
Tiago-Pessoa <tiago.pessoa@cpb.com.br>
2022-09-06 17:29:23 -0300

    aula34

--- src/app/core/interceptors/token.interceptor.ts
+++ src/app/core/interceptors/token.interceptor.ts
@@ -9,10 +9,26 @@ import { Observable } from 'rxjs';
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+    let token = localStorage.getItem('token');
 
-  constructor() {}
+     if (!token) {
+       token = this.generateToken();
+       localStorage.setItem('token', token);
+     }
+     request = request.clone({
+      setHeaders: {
+        Authorization: token
+      }
+     });
 
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     return next.handle(request);
+  }
+
+  private generateToken(): string {
+    return Math.random().toString(36).substring(2, 12);
   }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   // baseUrl: 'https://toh-json-server-api.herokuapp.com/api'
-  baseUrl: 'http://localhost:3000/api'
+  // baseUrl: 'http://localhost:3000/api'
+  baseUrl: 'https://curso-tour-of-heroes-api.herokuapp.com/api',
 };
